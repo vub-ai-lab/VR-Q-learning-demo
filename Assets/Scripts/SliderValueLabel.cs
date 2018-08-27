b2dc0b23e3e274f1f9324c672317b8fb64b71955
endymion64 <yocoppen@ai.vub.ac.be>
2018-08-27 15:32:17 +0200

    Additional forgotten scripts

---
+++ Assets/Scripts/SliderValueLabel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueLabel : MonoBehaviour
+{
+    public Slider m_Slider;
+    public Text m_Text;
+
+    void OnEnable()
+    {
+
+        m_Slider.onValueChanged.AddListener(delegate {
+            SliderValueChanged();
+        });
+
+        //Initialise the Text to say the first value of the Slider
+        m_Text.text = m_Slider.value.ToString("n2");
+    }
+
+    private void OnDisable()
+    {
+        m_Slider.onValueChanged.RemoveAllListeners();
+    }
+
+    //Output the new state of the Toggle into Text
+    void SliderValueChanged()
+    {
+        float value = m_Slider.value;
+        m_Text.text = value.ToString("n2");
+    }
+}
